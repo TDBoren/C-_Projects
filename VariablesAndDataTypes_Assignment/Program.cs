c10244d3f84ddfa53c6eb2b7e8645fd0f67270d0
TDBoren <63603319+TDBoren@users.noreply.github.com>
2020-07-18 09:41:39 -0600

    pg 71 assignment

---
+++ VariablesAndDataTypes_Assignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy \nStudent Daily Report");
+            Console.WriteLine("What is your name?");
+            Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            Console.ReadLine();
+            Console.WriteLine("What page number are you on?");
+            Console.ReadLine();
+            Console.WriteLine("Do you need help withy anything? Please answer \"true\" or \"false\".");
+            Console.ReadLine();
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            Console.ReadLine();
+            Console.WriteLine("Thank you for your answers. \nAn instructor will respond to this shortly. \nHave a great day!");
+            Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+        }
+    }
+}
