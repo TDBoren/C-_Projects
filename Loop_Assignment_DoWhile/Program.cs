95c0ad6d9684490abff5c5439cec206a9c0ec95e
TDBoren <63603319+TDBoren@users.noreply.github.com>
2020-08-10 12:09:52 -0600

    pg 121 commit

---
+++ Loop_Assignment_DoWhile/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_Assignment_DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Can you guess what number I am thinking of?\n\nTake a guess!.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 51;
+
+            do
+            {
+                switch (number)
+                {
+                    case 51:
+                        Console.WriteLine("You guessed the number 51. Amazing, that is the number I was thinking of!!!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You did not guess the number I am thinking of.");
+                        Console.WriteLine("\nTry again.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                }
+            }
+            while (!isGuessed);
+
+            Console.Read();
+        }
+    }
+}
---
+++ Loop_Assignment_While/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_Assignment_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Can you guess what number I am thinking of?\n\nTake a guess!.");
+        int number = Convert.ToInt32(Console.ReadLine());
+        bool isGuessed = number == 51;
+
+        while (!isGuessed)
+        {
+            switch (number)
+            {
+                case 51:
+                    Console.WriteLine("You guessed the number 51. amazing, that is the number I was thinking of!!!");
+                    isGuessed = true;
+                    break;
+                default:
+                    Console.WriteLine("You did not guess the number I am thinking of.");
+                    Console.WriteLine("\nTry again.");
+                    number = Convert.ToInt32(Console.ReadLine());
+                    break;
+
+            }
+        }
+        Console.Read();
+        }
+    }
+}
+
