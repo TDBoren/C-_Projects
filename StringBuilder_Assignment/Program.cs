f330aed0822d75a35c7215011caf105286c93009
TDBoren <63603319+TDBoren@users.noreply.github.com>
2020-08-10 13:15:21 -0600

    pg 125 commit

---
+++ StringBuilder_Assignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilder_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "My name is Troy. ";
+            string learning = "I am learning about strings";
+            string program = " using the C# program.";
+
+            Console.WriteLine(name + learning + program);
+            Console.ReadLine();
+
+            string uppercase = "Troy Boren";
+            uppercase = uppercase.ToUpper();
+            Console.WriteLine(uppercase);
+            Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Troy.");
+            sb.Append("My birthday is next month.");
+            sb.Append("I will graduate next month.");
+            sb.Append("I am using this to put a bunch of sentences together.");
+            sb.Append("This is for a StringBuilder exercise.");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ String_Functions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Jesse";
+            //string quote = "The man said, \"Hello\", Jesse. \n Hello on a new line. \n \t Hello on a new tab.";
+            //string fielName = @"C:\\Users\\Jesse";
+
+            //bool trueOrFalse = name.Contains("s");
+            //trueOrFalse = name.EndsWith("s");
+
+            //int length = name.Length;
+
+            //name = name.ToUpper();
+
+            //name = name.ToLower();
+
+            //Console.WriteLine(trueOrFalse);
+            //Console.ReadLine();
+
+            //string name = "Jesse";
+            //name = "Joe";
+
+            //Console.WriteLine(name);
+            //Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Jesse");
+
+        }
+    }
+}
