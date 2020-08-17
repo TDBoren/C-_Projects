8a604e1e30a8fce8f11208062228d7f0a9a43465
TDBoren <63603319+TDBoren@users.noreply.github.com>
2020-08-17 05:07:03 -0600

    pg 152 commit

---
+++ 11Part_Array_Assignment/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11Part_Array_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Assignment part 1
+
+            string[] cars = new string[3];
+            for (int counter = 0; counter < cars.Length; counter++)
+            {
+                Console.WriteLine("Please enter in the name of a car.");
+                cars[counter] = Console.ReadLine();
+            }
+            foreach (string car in cars)
+            {
+                Console.WriteLine(car);
+            }
+            Console.ReadLine();
+ 
+            
+            
+                
+            // Assignment part 2 - 5
+            // a-- creates the infinite loop
+            // a < 50 changed to <= 50 for assignment progression
+            for (int a = 0; a <= 50; a++)
+            {
+                Console.WriteLine("value : {0}", a);
+            }
+            Console.ReadLine();
+
+            // Assignment part 6 - 8
+            List<string> space = new List<string> { "planets", "stars", "comets", "shuttle" };
+            for (int n = 0; n < space.Count; n++)
+            {
+                Console.WriteLine(space[n]);
+            }
+            Console.ReadLine();
+            Console.WriteLine("Please choose an item from the list.");
+            string userInput = Console.ReadLine();
+            foreach (string n in space)
+            {
+                if (userInput == n)
+                {
+                    Console.WriteLine("You have chosen " + userInput);
+                }
+                else 
+                {
+                    if (userInput != n)
+                    {
+                        Console.WriteLine("You did not select an item from the list.");
+                    }    
+                }
+            }
+            
+            List<string> pets = new List<string>();
+            pets.Add("a bird");
+            pets.Add("a horse");
+            pets.Add("a dog");
+            pets.Add("a cat");
+            pets.Add("a turtle");
+            pets.Add("a cat");
+
+            for (int n = 0; n < pets.Count; n++)
+            {
+                Console.WriteLine("Enter {0} to select {1}", n, pets[n]);
+            }
+            string numberChoice = Console.ReadLine();
+            int c = int.Parse(numberChoice);
+            Console.WriteLine("You have selected {1} for your pet.\nThank you.", c, pets[c]);
+            Console.ReadLine();
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExceptionHandling_Assignment/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numArray = { 5, 20, 10, 200, 5000, 600, 2300 };
+
+            for (int n = 0; n < numArray.Length; n++)
+            {
+                Console.WriteLine(numArray[n]);
+            }
+            Console.WriteLine("Please type in a number to divide by.");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            for (int n = 0; n < numArray.Length; n++)
+            {
+                var divide = numArray[n] / userNumber;
+                Console.WriteLine("Your answers are " + divide);
+            }
+            try
+            {
+                int[] numArray2 = { 5, 20, 10, 200, 5000, 600, 2300 };
+
+                for (int n = 0; n < numArray2.Length; n++)
+                {
+                    Console.WriteLine(numArray2[n]);
+                }
+                Console.WriteLine("Please type in a number to divide by.");
+                int userNumber2 = Convert.ToInt32(Console.ReadLine());
+
+                for (int n = 0; n < numArray2.Length; n++)
+                {
+                    var divide = numArray2[n] / userNumber2;
+                    Console.WriteLine("Your answers are " + divide);
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number.");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("You can't divide by a zero.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+               Console.ReadLine();
+            }
+            Console.WriteLine("You have reached the end of the program.");
+            Console.ReadLine();
+        }
+    }
+}
