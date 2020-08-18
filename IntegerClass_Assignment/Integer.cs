6e24210f5e08e6ea910ecaf009bae0c83e6e36ed
TDBoren <63603319+TDBoren@users.noreply.github.com>
2020-08-18 06:44:30 -0600

    pg 161 commit

---
+++ IntegerClass_Assignment/Integer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerClass_Assignment
+{
+    
+    class Integer
+    {
+        public int Multiply;
+        public int Add;
+        public int Divide;
+        public int Subtract;
+    }
+}
