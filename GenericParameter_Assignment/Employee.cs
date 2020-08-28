90e285b1083c1dc5896bbaa1dbb4047cd57cac34
TDBoren <63603319+TDBoren@users.noreply.github.com>
2020-08-28 05:27:38 -0600

    pg248 submit

---
+++ GenericParameter_Assignment/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericParameter_Assignment
+{
+    public class Employee<T>
+    {
+        private List<T> nodes;
+
+        public Employee()
+        {
+            this.nodes = new List<T>();
+        }
+
+        public void AddNode(T newNode)
+        {
+            nodes.Add(newNode);
+        }
+
+        public void ProcessAllNodes()
+        {
+            foreach (var node in nodes)
+            {
+                Console.WriteLine(node.ToString());
+            }
+        }
+    }
+}
---
+++ GenericParameter_Assignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericParameter_Assignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee<int> Age = new Employee<int>();
+            Age.AddNode(21);
+            Age.AddNode(46);
+            Age.AddNode(36);
+            Age.AddNode(67);
+            Age.AddNode(55);
+
+            Age.ProcessAllNodes();
+
+            Employee<string> Name = new Employee<string>();
+            Name.AddNode("Lory");
+            Name.AddNode("Julia");
+            Name.AddNode("Lerman");
+            Name.AddNode("James");
+
+            Name.ProcessAllNodes();
+            Console.ReadLine();
+        }
+    }
+}
