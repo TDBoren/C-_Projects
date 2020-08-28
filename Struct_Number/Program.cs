87315b210a9383d9d6f767e79986cdf1188279e4
TDBoren <63603319+TDBoren@users.noreply.github.com>
2020-08-28 11:31:28 -0600

    pg 260 commit

---
+++ Struct_Number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number a;
+            a.Amount = 50.00m;
+
+            Console.WriteLine(a.Amount);
+            Console.ReadLine();
+        }
+    }
+}
