093df22dedbdead87f93e14fe0a6d2d7ba5e0247
TDBoren <63603319+TDBoren@users.noreply.github.com>
2020-09-02 09:25:53 -0600

    pg 293 commit

---
+++ DateTime_Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine("The current time is: " + currentTime);
+            Console.ReadLine();
+            Console.WriteLine("Please enter a number: ");
+            double userNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(currentTime.AddHours(userNumber));
+            Console.ReadLine();
+        }
+    }
+}
